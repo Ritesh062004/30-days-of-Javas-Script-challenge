2330c2a4f95a5ca7dae5d0974da0f7dca396590e
Ritesh Kumar Singh <singhritesh0016@gmail.com>
2025-12-24 08:51:48 +0000

    variable puss in basics

---
+++ .devcontainer/day1_BasicsJS/1_Variables.js
@@ -0,0 +1,19 @@
+//const name = "Ritesh"
+//console.log(name)
+
+//let name = "Ritesh"
+//console.log();
+
+//var
+var name ="Ritesh"
+name = "yash"
+var name = "ram"
+console.log(name);
+
+{
+int name = "Ritesh"
+
+   int name = "yesh"
+   console.log(name);
+}
+
